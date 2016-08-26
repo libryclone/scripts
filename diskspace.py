77853bf10c06d6910c2caf337bc74bf06a49defe
Alex Grintsvayg <alex@grin.io>
2016-08-26 10:39:36 -0400

    script to notify us when disk space runs low

---
+++ diskspace.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+import psutil
+import requests
+import socket
+import sys
+
+partitions_to_check = ['/', '/home']
+threshold = 80  # alert if disk is fuller than this many percent
+
+slack_url = os.environ.get('SLACK_URL')
+if not slack_url:
+    sys.stderr.write('Error: SLACK_URL env var not found\n')
+    sys.exit(1)
+
+for partition in psutil.disk_partitions():
+    if (
+            partition.mountpoint in partitions_to_check and
+            psutil.disk_usage(partition.mountpoint).percent > threshold
+    ):
+        payload = {
+            'username': 'diskspace',
+            'icon_emoji': ':minidisc:',
+            'channel': '#tech',
+            'text': (
+                socket.gethostname() + ': disk at ' + partition.mountpoint +
+                ' is ' + str(psutil.disk_usage(partition.mountpoint).percent) +
+                '% full'
+            )
+        }
+        r = requests.post(slack_url, json=payload)
